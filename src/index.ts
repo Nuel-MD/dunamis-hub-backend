39de173af6f9f6c2b141bac583d2823d1bbadea2
Muogbo Emmanuel <muogboemmanuel9@gmail.com>
2025-05-21 16:27:27 +0100

    api for pagination

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,24 @@ config();
 const app: Express = express();
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://dunamisfaith.vercel.app",
+        process.env.FRONTEND_URL,
+      ].filter(Boolean);
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -39,7 +56,13 @@ const swaggerOptions = {
       version: "1.0.0",
       description: "API for Dunamis Faith Hub platform",
     },
-    servers: [{ url: `http://localhost:${process.env.PORT || 5000}` }],
+    servers: [
+      {
+        url:
+          `http://localhost:${process.env.PORT || 5000}` ||
+          "https://dunamis-hub.onrender.com",
+      },
+    ],
   },
   apis: ["./src/routes/*.ts", "./src/controllers/*.ts"],
 };
